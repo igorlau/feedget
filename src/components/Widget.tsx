c3c376711807234befa314fe8a2008800cb881bc
igorlau <igorassislau@gmail.com>
2022-05-04 18:01:01 -0300

    setting up prettier configs

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,19 +1,18 @@
-import { ChatTeardropDots} from 'phosphor-react';
-import { Popover } from '@headlessui/react'
+import { ChatTeardropDots } from 'phosphor-react';
+import { Popover } from '@headlessui/react';
 
 export const Widget = () => {
-    return (
-        <Popover className="absolute right-5 bottom-5">
-            <Popover.Panel>Hello World</Popover.Panel>
-            <Popover.Button
-                className="flex items-center bg-brand-500 rounded-full px-3 h-12 text-white group">
-                <ChatTeardropDots className="w-6 h-6" />
+  return (
+    <Popover className="absolute right-5 bottom-5">
+      <Popover.Panel>Hello World</Popover.Panel>
+      <Popover.Button className="flex items-center bg-brand-500 rounded-full px-3 h-12 text-white group">
+        <ChatTeardropDots className="w-6 h-6" />
 
-                <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
-                    <span className="pl-2"></span>
-                    Feedback
-                </span>
-            </Popover.Button>
-        </Popover>
-    );
-}
+        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+          <span className="pl-2"></span>
+          Feedback
+        </span>
+      </Popover.Button>
+    </Popover>
+  );
+};
